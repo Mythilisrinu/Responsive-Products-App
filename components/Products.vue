37b5b8764cb4b8cc637cf44f237190cf2b24c37d
Mythili S <mythilisrinu6@gmail.com>
2026-04-18 16:59:17 +0530

    Add loading spinner and message while fetching products

--- components/Products.vue
+++ components/Products.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="products">
-        <Product v-for="product in products" :key="product.id" :product="product" />    
+        <div v-if="loading" class="loader">
+            <div class="spinner"></div>
+            <h3>Loading products...</h3>
+        </div>
+        <Product v-for="product in products" :key="product.id" :product="product" v-else />
     </div>
 </template>
 <script>
@@ -12,7 +16,8 @@ export default {
   },
   data() {
     return {
-      products: []
+      products: [],
+      loading: false
     }
   },
   mounted() {
@@ -20,12 +25,15 @@ export default {
   },
   methods: {
     async fetchProducts() {
+      this.loading = true;
       try {
         const response = await fetch('https://fakestoreapi.com/products');
         const data = await response.json();
         this.products = data;
       } catch (error) {
         console.error('Error fetching products:', error);
+      } finally {
+        this.loading = false;
       }
     }
   }
@@ -38,5 +46,28 @@ export default {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-evenly;
+}
+
+.loader {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 80vh;
+    width: 100%;
+}
+
+.spinner {
+    border: 6px solid #f3f3f3;
+    border-top: 6px solid #3498db;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
 }
 </style>
